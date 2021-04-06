d14f1c1952af85304f2fde630b481dae3e93baa2
Caio Lemec <caio.lemec@gmail.com>
2021-04-06 20:28:50 -0300

    Inserindo transação na API

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import {createServer} from 'miragejs';
+import {createServer, Model} from 'miragejs';
 
 createServer({
+	models: {
+		transaction: Model,
+	},
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: 'Freelance de website',
+					type: 'deposit',
+					category: 'Dev',
+					amount: 6000,
+					createdAt: new Date('2021-02-13 09:20:00'),
+				},
+				{
+					id: 2,
+					title: 'Pedido Ifood',
+					type: 'withdraw',
+					category: 'Comida',
+					amount: 150,
+					createdAt: new Date('2021-02-13 09:50:00'),
+				}
+			],
+		})
+	},
 	routes() {
 		this.namespace = 'api'; 
 
 		this.get('/transactions', ()=> {
-			return [
-				{
-					id: 1,
-					title: 'Transaction 1',
-					amount: 400,
-					type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-				}
-			]
+			return this.schema.all('transaction');
+		})
+
+		this.post('/transactions', (schema, request) => {
+			const data = JSON.parse(request.requestBody)
+			return schema.create('transaction', data)
 		})
 	}
 })
